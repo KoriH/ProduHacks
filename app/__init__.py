3eebc1150aafefbe870fea49d057cdff7bf9b43f
Skidlez <96402323+korihuen@users.noreply.github.com>
2024-03-23 21:12:39 -0700

    this works. index done

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 import os
 
 template_dir = os.path.abspath('app/client/templates')
-app = Flask(__name__, template_folder=template_dir)
+static_dir = os.path.abspath('app/client/static')
+app = Flask(__name__, template_folder=template_dir,static_folder=static_dir)
 
 from app.client import views
