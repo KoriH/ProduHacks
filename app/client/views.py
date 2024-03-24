f7a152ac6ac897622a168ae6fd74af9dd625914d
Skidlez <96402323+korihuen@users.noreply.github.com>
2024-03-23 22:19:58 -0700

    basics

--- app/client/views.py
+++ app/client/views.py
@@ -4,3 +4,15 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/loading_page', methods=['GET'])
+def loading_page():
+    return render_template('loading.html')
+
+@app.route('/list', methods=['GET'] )
+def list():
+    return render_template('list.html')
+
+@app.route('/videos', methods=['GET'] )
+def videos():
+    return render_template('videos.html')
