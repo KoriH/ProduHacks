c121006a5e8887c212e69e34b64c513242664042
Ishaan Datta <Ishaandatta737@gmail.com>
2024-03-23 22:48:04 -0700

    minimum viable product

--- app/models/camera.py
+++ app/models/camera.py
@@ -3,7 +3,6 @@ import supervision as sv
 import numpy as np
 import os
 import cv2
-import torch
 
 os.chdir('C:/Users/ishaa/Downloads')
 
@@ -17,6 +16,11 @@ velocities = {}
 boxes = {}
 frames = {}
 
+# decord find how many frames/second and then downsample to 30fps to prevent stuttering?
+# verify operation on computer
+# velocity recording unstable
+# turn up contrast
+
 def compute_velocity(tracker_id, centroid_x, centroid_y):
     prev_x, prev_y = centroids[tracker_id]
     velocity = np.sqrt((centroid_x - prev_x) ** 2 + (centroid_y - prev_y) ** 2)
@@ -24,9 +28,10 @@ def compute_velocity(tracker_id, centroid_x, centroid_y):
     # conversion of frames to seconds
     velocities[tracker_id] = velocity
 
-def annotate_frame(frame, x1, y1, x2, y2, tracker_id, centroid_x, centroid_y):
-    frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    frame = cv2.draw_text(frame, text=f"{velocities[tracker_id]}", position=(centroid_x, centroid_y), font=cv2.FONT_HERSHEY_SIMPLEX, size=0.9, color=(0, 255, 0), thickness=2)
+def annotate_frame(frame, x1, y1, x2, y2, tracker_id):
+    x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
+    frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 2)
+    frame = cv2.putText(frame, f"{velocities[tracker_id]}", (x2+7, y2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 255), 1)
     return frame
 
 while vidObj.isOpened():
@@ -35,26 +40,22 @@ while vidObj.isOpened():
     
     if success:
         frame_id += 1
+        uncropped = frame.copy()
         frame = frame[220:530, :]
         results = model(frame)[0]
         detections = sv.Detections.from_ultralytics(results)
         detections = tracker.update_with_detections(detections)
-
-        print(repr(detections))
-        print(detections.tracker_id)
         
         for tracker_id, box in zip(detections.tracker_id, detections.xyxy):
             x1, y1, x2, y2 = box #[0]
-            print(x1, x2 ,y1, y2)
+            y1, y2 = y1 + 220, y2 + 220
             boxes[tracker_id] = (x1, y1, x2, y2)
             
             centroid_x = (x1 + x2) / 2
             centroid_y = (y1 + y2) / 2
-            if tracker_id in centroids:
-                compute_velocity(tracker_id, centroid_x, centroid_y)
-            
-            annotated_frame = annotate_frame(frame, x1, y1, x2, y2, tracker_id, centroid_x, centroid_y)
             centroids[tracker_id] = (centroid_x, centroid_y)
+            compute_velocity(tracker_id, centroid_x, centroid_y)
+            annotated_frame = annotate_frame(uncropped, x1, y1, x2, y2, tracker_id)
             frames[tracker_id] = frame_id
         
         # reverse y comparisons
@@ -71,11 +72,6 @@ while vidObj.isOpened():
         # Break the loop if the end of the video is reached
         break
 
-
-
 # Release the video capture object and close the display window
 vidObj.release()
 cv2.destroyAllWindows()
-
-# possible detection at (image coordinate) + boxes
-# turn up contrast
