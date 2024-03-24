5dbe24713c90400f4eed0c104ab340ab4cbb9cdb
Ishaan Datta <Ishaandatta737@gmail.com>
2024-03-23 22:20:50 -0700

    lol!

--- camera.py
+++ camera.py
@@ -2,29 +2,80 @@ from ultralytics import YOLO
 import supervision as sv
 import numpy as np
 import os
-from decord import VideoReader, cpu
+import cv2
+import torch
 
 os.chdir('C:/Users/ishaa/Downloads')
 
-vr = VideoReader('test.mp4', ctx=cpu(0))
-print('video frames:', len(vr))
-for i in range(len(vr)):
-    frames = vr.get_batch(range(i*BATCH_SIZE, (i+1)*BATCH_SIZE)).asnumpy()
-    print(frames.shape)
-    print(frames.dtype)
-    print(frames.context)
-
 model = YOLO("yolov8n.pt")
+tracker = sv.ByteTrack()
+vidObj = cv2.VideoCapture('test.mp4') 
 
-box_annotator = sv.BoundingBoxAnnotator()
+frame_id = 0
+centroids = {}
+velocities = {}
+boxes = {}
+frames = {}
 
-def callback(frame: np.ndarray, _: int) -> np.ndarray:
-    results = model(frame)[0]
-    detections = sv.Detections.from_ultralytics(results)
-    return box_annotator.annotate(frame.copy(), detections=detections)
+def compute_velocity(tracker_id, centroid_x, centroid_y):
+    prev_x, prev_y = centroids[tracker_id]
+    velocity = np.sqrt((centroid_x - prev_x) ** 2 + (centroid_y - prev_y) ** 2)
+    # velocity = centroid_x - prev_x / frames[tracker_id]
+    # conversion of frames to seconds
+    velocities[tracker_id] = velocity
 
-sv.process_video(
-    source_path="test.mp4",
-    target_path="result.mp4",
-    callback=callback
-)
+def annotate_frame(frame, x1, y1, x2, y2, tracker_id, centroid_x, centroid_y):
+    frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    frame = cv2.draw_text(frame, text=f"{velocities[tracker_id]}", position=(centroid_x, centroid_y), font=cv2.FONT_HERSHEY_SIMPLEX, size=0.9, color=(0, 255, 0), thickness=2)
+    return frame
+
+while vidObj.isOpened():
+    # Read a frame from the video
+    success, frame = vidObj.read()
+    
+    if success:
+        frame_id += 1
+        frame = frame[220:530, :]
+        results = model(frame)[0]
+        detections = sv.Detections.from_ultralytics(results)
+        detections = tracker.update_with_detections(detections)
+
+        print(repr(detections))
+        print(detections.tracker_id)
+        
+        for tracker_id, box in zip(detections.tracker_id, detections.xyxy):
+            x1, y1, x2, y2 = box #[0]
+            print(x1, x2 ,y1, y2)
+            boxes[tracker_id] = (x1, y1, x2, y2)
+            
+            centroid_x = (x1 + x2) / 2
+            centroid_y = (y1 + y2) / 2
+            if tracker_id in centroids:
+                compute_velocity(tracker_id, centroid_x, centroid_y)
+            
+            annotated_frame = annotate_frame(frame, x1, y1, x2, y2, tracker_id, centroid_x, centroid_y)
+            centroids[tracker_id] = (centroid_x, centroid_y)
+            frames[tracker_id] = frame_id
+        
+        # reverse y comparisons
+        for box in boxes:
+            pass
+            # help quant tree????
+            
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+
+
+# Release the video capture object and close the display window
+vidObj.release()
+cv2.destroyAllWindows()
+
+# possible detection at (image coordinate) + boxes
+# turn up contrast
